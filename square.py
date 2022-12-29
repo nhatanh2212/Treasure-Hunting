5cf9dedfa31d61d9907afd8e5838e9636bd866e4
Truong Nhat Anh <tnanh20@apcs.fitus.edu.vn>
2022-12-29 21:06:52 +0700

    update square : check around

--- square.py
+++ square.py
@@ -6,3 +6,24 @@ class Square:
 
         def has_piece(self):
                 return self.piece != None
+        
+        def __eq__(self, other):
+                return self.row == other.row and self.col == other.col
+
+        def has_piece(self):
+                if self.piece.name != "Normal":
+                        return True
+                else:
+                        return False
+
+        def isempty(self):
+                return not self.has_piece()
+
+        def has_team_piece(self,name):
+                return self.has_piece() and self.piece.name == name
+
+        def has_enemy_piece(self, name):
+                return self.has_piece() and self.piece.name != name
+
+        def isempty_or_enemy(self, name):
+                return self.isempty() or self.has_enemy_piece(name)
